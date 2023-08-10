738aa93ee70b0fa524cd86147e60160f3994d223
Asande Didiza <asande.didiza@gmail.com>
2023-08-10 09:59:49 -0700

    Created c file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a sentence
+ * Return: returns 0 if the program is successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
