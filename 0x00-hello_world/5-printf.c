88ce23121f11abdeecf6556221880f5968af7643
Asande Didiza <asande.didiza@gmail.com>
2023-08-10 10:07:40 -0700

    Created 5-printf.c C file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** 
+ * main - this will print a sentence
+ * Return: THis returns 0 if programs runs without errors
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
