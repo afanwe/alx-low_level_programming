882fb70e5221728677ed650d85393c7acbd804b7
Asande Didiza <asande.didiza@gmail.com>
2023-08-11 14:35:12 -0700

    Created 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this will print all alphabets but q and e
+ * Return: returns 0 upon successful execution
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (!(alph == 'q') || !(alph == 'e'))
+			putchar(alph);
+	}
+	putchar('\n');
+
+	return (0);
+}
