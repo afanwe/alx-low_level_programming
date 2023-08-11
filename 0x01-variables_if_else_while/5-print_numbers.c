60681341a79ad6e101d5dd194f180a12b7a512c0
Asande Didiza <asande.didiza@gmail.com>
2023-08-11 15:35:38 -0700

    Created 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints base 10 numbers
+ * Return: return 0 upon successful run
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+	printf("\n");
+
+	return (0);
+}
