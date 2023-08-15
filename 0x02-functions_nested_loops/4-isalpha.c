0f5dcb1ee41f6041d25031daa19cc527905c0b58
Asande Didiza <asande.didiza@gmail.com>
2023-08-15 10:54:28 -0700

    Created 4-main.c and 4-isalpha.c files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if the character is an alphabet or not
+ * @c: is our parameter for the function
+ * Return: returns 1 if c is an alphabet
+ */
+
+int _isalpha(int c)
+{
+	int result;
+
+	if (( c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
+		result = 1;
+	else
+		result = 0;
+
+	return (result);
+}
