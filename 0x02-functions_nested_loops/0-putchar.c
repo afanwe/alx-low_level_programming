85641209d53a1d90f24d965b59d86f9648524002
Asande Didiza <asande.didiza@gmail.com>
2023-08-15 04:35:17 -0700

    CReated new directory 0x02-functions_nested_loops, README.md and 0-putchar.c files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - This print text
+ * Return: returnsx 0 upon successful execution
+ */
+
+int main(void)
+{
+	_putchar('_putchar');
+	return (0);
+}
