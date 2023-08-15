0591eac023eeceb2e4de5dc3f86c1dd338a06f2f
Asande Didiza <asande.didiza@gmail.com>
2023-08-15 15:09:03 -0700

    Created c files with code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adding two numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: returns sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks code and execute
+ * Return: returns 0 upon successful execution
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+
+	return (0);
+}
