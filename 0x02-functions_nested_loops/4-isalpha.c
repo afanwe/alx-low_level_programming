a43fee8fef46fd383eb11a1c739ccb88bfe9d3e5
Asande Didiza <asande.didiza@gmail.com>
2023-08-15 11:00:51 -0700

    Removed spaces in code for 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,7 +10,7 @@ int _isalpha(int c)
 {
 	int result;
 
-	if (( c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		result = 1;
 	else
 		result = 0;
