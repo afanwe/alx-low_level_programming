e5661d59b9f0e15172a033d024d162b475e54cf6
Asande Didiza <asande.didiza@gmail.com>
2023-08-15 14:51:35 -0700

    Created a few c files

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks the code and executes
+ * Return: returns 0 upon successful execution
+ */
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - checks the code and executes
+ * Return: returns 0 upon successful execution
+ */
+
+int main(void)
+{
+	jack_bauer();
+
+	return (0);
+}
