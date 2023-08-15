16d5376664e1fd4a03197780247fb2697a4c3e6f
Asande Didiza <asande.didiza@gmail.com>
2023-08-15 08:11:18 -0700

    CReated 2-main.c 2-print_alphabet_x10.c files

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - calls the print_alphabet function
+ * Return: returns 0 upon successfull execution
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
