aedaa14e4ee83f61110beafcd8d888526217d4b9
Asande Didiza <asande.didiza@gmail.com>
2023-08-16 02:35:07 -0700

    Created 102-fibonacci.c file and fixed issues of whitespaces on 101-natural.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci sequence numbers
+ * Return: Returns 0 upon successful execution
+ */
+
+int main(void)
+{
+	long int num1 = 1;
+	long int num2 = 2;
+	long int nextnum;
+	int i;
+
+	for (i = 0; i < 51; i++)
+	{
+		nextnum = num1 + num2;
+		if (i != 50)
+			printf("%li, ", num1);
+		else
+			printf("%li\n", num1);
+		num1 = num2;
+		num2 = nextnum;
+	}
+
+	return (0);
+}
