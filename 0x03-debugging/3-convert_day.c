06e6c258383accc49bdf009fb9e3abb6a893d857
Asande Didiza <asande.didiza@gmail.com>
2023-08-17 06:50:10 -0700

    Fixed code

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+int convert_day(int month, int day)
+{
+	switch (month)
+	{
+		case 2:
+			day = 31 + day;
+			break;
+			
