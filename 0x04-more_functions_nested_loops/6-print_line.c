19abf360e54a010de669a3403ef0dd786a4f7be2
Asande Didiza <asande.didiza@gmail.com>
2023-08-17 12:05:43 -0700

    Rewrote code in file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,16 @@ void print_line(int n)
 {
 	int i;
 
-	if (n < 1)
-		_putchar('\n');
-	else
+	if (!(n < 1))
+	{
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 	_putchar('\n');
 }
