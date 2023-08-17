c5997109eb52b520f598a108cd9ed2c7b67b1167
Asande Didiza <asande.didiza@gmail.com>
2023-08-17 04:35:58 -0700

    Recreated 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests the function that prints if a integer is pos,neg or 0
- * Return: returns 0 upon successful
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
