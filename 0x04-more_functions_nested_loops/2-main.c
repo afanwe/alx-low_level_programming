f4a41778a096d27abd491be3c9dd33600628a832
Asande Didiza <asande.didiza@gmail.com>
2023-08-17 09:10:40 -0700

    Created 2-mul.c and 2-main.c files

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - will display the multiple of the integers
+ * Return: 0 if runs with no error
+ */
+
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - will multiply the two integers
+ * @a: first interger
+ * @b: second interger
+ * Return: returns multiple of the intergers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
