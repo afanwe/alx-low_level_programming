a9ca511f0437c614630d3b74faaa91ebb0d6e5b8
Asande Didiza <asande.didiza@gmail.com>
2023-08-17 11:35:37 -0700

    Created 6-print_line.c and 6-main.c

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints lines as per input
+ * Return: 0 if runs with no error
+ */
+
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints _ as per user input
+ * @n: will determine how many _ to print
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (!(n < 1))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	else
+		_putchar(' ');
+	_putchar('\n');
+}
