8842e33779d0c134c7b99670ba096480da805b26
Asande Didiza <asande.didiza@gmail.com>
2023-08-17 13:17:11 -0700

    Created 8-print_square.c and 8-main.c files

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - prints square pattern
+ * @size: will determine size of square
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+	int n;
+
+	n = size;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for(j = 0; j < n; j++)
+			{
+				_putchar('#');
+
+			}
+			_putchar('\n');
+		}
+	}
+}
+
