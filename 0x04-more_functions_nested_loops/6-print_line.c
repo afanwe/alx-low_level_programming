512af6e6a41a87b8bb17a82eccb311587c3194e0
Asande Didiza <asande.didiza@gmail.com>
2023-08-17 11:40:00 -0700

    Fixed code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (!(n < 1))
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
-	else
-		_putchar(' ');
 	_putchar('\n');
 }
