daec7c9e6550e58efde9c96a6054636af539fb7f
Asande Didiza <asande.didiza@gmail.com>
2023-08-18 05:24:41 -0700

    Created 9-fizz_buzz.c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - checks code for errors and executes
+ * Return: 0 if code runs without errors
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) != 0 && (i % 5) != 0)
+		{
+			printf("%d ", i);
+		}
+		else if ((i % 3) == 0 && (i % 5) != 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 3) != 0 && (i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("FizzBuzz ");
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
