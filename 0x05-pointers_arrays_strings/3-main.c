8de26b0a0af206f2619cfc955aac4be3a3abc427
Asande Didiza <asande.didiza@gmail.com>
2023-08-21 03:04:20 -0700

    Created 3-main.c and 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - checks code and executes
+ * Return: 0 upon successful execution
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
