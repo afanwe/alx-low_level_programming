49521095e45f9ed48c35f6d2ecb94d928d8fb484
Asande Didiza <asande.didiza@gmail.com>
2023-08-21 03:37:04 -0700

    Created 4-main.c and 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - checks and executes code
+ * Return: 0 upon successful execution
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
