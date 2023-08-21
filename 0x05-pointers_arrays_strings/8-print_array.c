1aa3634bc95075b4deab45ca3678cbaa8e4bfb5e
Asande Didiza <asande.didiza@gmail.com>
2023-08-21 13:17:12 -0700

    CReated 8-main.c file and 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - will print all elements of array
+ * @a: will store the arrary
+ * @n: will store size of the arrary
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if ( i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	putchar('\n');
+}
