b0dfd375e95f3a55259efb2930ab46b46d343f15
Asande Didiza <asande.didiza@gmail.com>
2023-08-21 14:02:49 -0700

    Edited code

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -19,12 +19,11 @@ char *_strcpy(char *dest, char *src)
 		length++;
 	}
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; i < length; i++)
 	{
 		dest[i] = src[i];
 	}
 
 	dest[length + 1] = '\0';
-
 	return (dest);
 }
