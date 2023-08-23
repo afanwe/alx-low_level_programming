f41e777715addfbd274d0bd901260baa31239be6
Asande Didiza <asande.didiza@gmail.com>
2023-08-23 10:58:27 -0700

    CReated 2-main.c and 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - will replace dest with src at n
+ * @dest: first string
+ * @src: second string
+ * @n: will be how many characters to copy
+ * Return: returns new string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n )
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
