a0a79d754e074f1cccf617225627bfecc467375b
Asande Didiza <asande.didiza@gmail.com>
2023-08-24 05:44:40 -0700

    Fixed code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,15 +11,15 @@ char *leet(char *str)
 	int a, b;
 
 	char alpha[] = { 'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L' };
-	char numbers[] = "4433007711";
-
+	char numbers[] = { 4, 4, 3, 3, 0, 0, 7, 7, 1, 1 };
+	
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
 			if (str[a] == alpha[b])
 			{
-				str[a] = numbers[b];
+				str[a] = numbers[b] + '0';
 			}
 		}
 	}
