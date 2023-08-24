af1160244479636a7c2ca18fce2e0e22a06cef9b
Asande Didiza <asande.didiza@gmail.com>
2023-08-24 03:31:06 -0700

    CReated 1-main.c and 1-strncat.c files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncat - concatenates n characters of src to dest
+ * @dest: first string
+ * @src: second string
+ * @n: numbers of characters to append
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, dest_len;
+
+	dest_len = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest_len++;
+	}
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+
+	dest[dest_len] = '\0';
+
+	return (dest);
+}
