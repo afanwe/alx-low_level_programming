c13dd78593e3026440b67d364cf2d79d19719d05
Asande Didiza <asande.didiza@gmail.com>
2023-08-24 05:47:41 -0700

    Removed whitespaces in code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ char *leet(char *str)
 
 	char alpha[] = { 'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L' };
 	char numbers[] = { 4, 4, 3, 3, 0, 0, 7, 7, 1, 1 };
-	
+
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		for (b = 0; b < 10; b++)
