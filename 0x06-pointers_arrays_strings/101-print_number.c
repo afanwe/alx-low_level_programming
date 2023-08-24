8bf0de95e6c7c060d85063128bb6cfcb93a3f434
Asande Didiza <asande.didiza@gmail.com>
2023-08-24 11:04:28 -0700

    Removed if statement that prints zero

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,16 +15,9 @@ void print_number(int n)
 		_putchar('-');
 		num = n * -1;
 	}
-
-	if (num == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		if (num / 10)
-			print_number(num / 10);
-	}
+	
+	if (num / 10 != 0)
+		print_number(num / 10);
 
 	_putchar(num % 10 + '0');
 }
