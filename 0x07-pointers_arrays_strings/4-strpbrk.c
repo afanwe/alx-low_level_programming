11950e4296f6b4ef24fdd504c392d416f7405582
Asande Didiza <asande.didiza@gmail.com>
2023-08-28 05:42:43 -0700

    Fixed code

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -18,11 +18,11 @@ char *_strpbrk(char *s, char *accept)
 		return  (NULL);
 	}
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		tmp = accept;
 
-		for (j = 0; tmp[j] >= '\0'; j++)
+		for (j = 0; tmp[j] != '\0'; j++)
 		{
 			if (s[i] == tmp[j])
 			{
