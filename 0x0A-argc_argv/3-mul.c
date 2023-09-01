8e502a7757e8315a6bf3b5ea5f5485b0f42754eb
Asande Didiza <asande.didiza@gmail.com>
2023-09-01 09:27:14 -0700

    Fixed betty errors

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,38 +19,27 @@ int _string_to_int(char *str)
 	num = 0;
 
 	while (str[length] != '\0')
-	{
 		length++;
-	}
-
 	while (i < length && k == 0)
 	{
 		if (str[i] == '-')
 			++j;
-
 		if (str[i] >= '0' && str[i] <= '9')
 		{
 			num = str[i] - '0';
 			if (j % 2)
-			{
 				num = -num;
-			}
 			n = n * 10 + num;
 			k = 1;
 
 			if (str[i + 1] < '0' || str[i + 1] > '9')
-			{
 				break;
-			}
 			k = 0;
 		}
 		i++;
 	}
-
 	if (k == 0)
-	{
 		return (0);
-	}
 	return (n);
 }
 
@@ -65,15 +54,9 @@ int main(int argc, char *argv[])
 {
 	if (argc < 3 || argc > 3)
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
+		printf("Error\n");
 		return (1);
 	}
-
 	printf("%d\n", _string_to_int(argv[1]) * _string_to_int(argv[2]));
 	return (0);
 }
