dc383abfc68529ef8b9064fb54466b018c0775ce
Asande Didiza <asande.didiza@gmail.com>
2023-09-01 05:41:12 -0700

    added code to 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,4 +8,24 @@
 
 int _atoi(char *s)
 {
+	int symb;
+	int i;
+
+	i = 0;
+	symb = 1;
+	unsigned int res = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			symb*= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= symb;
+	return (res);
 }
