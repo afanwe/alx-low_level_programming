7caaf3074ba5823d8201cbda0e06010c4a5b5efc
Asande Didiza <asande.didiza@gmail.com>
2023-09-04 05:32:59 -0700

    Fixed code to pass NULL cases

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,41 +14,40 @@ char *str_concat(char *s1, char *s2)
 	int i, j, s1_len, s2_len;
 	char *new_str;
 
-	if (s1 != NULL || s2 != NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	s1_len = 0;
+	s2_len = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		s1_len = 0;
-		s2_len = 0;
+		s1_len++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		s2_len++;
+	}
+	new_str = (char *) malloc(sizeof(char) * (s2_len + s1_len + 1));
 
-		for (i = 0; s1[i] != '\0'; i++)
+	if (new_str != NULL)
+	{
+		for (i = 0; i < s1_len; i++)
 		{
-			s1_len++;
+			new_str[i] = s1[i];
 		}
-		for (i = 0; s2[i] != '\0'; i++)
+		j = 0;
+
+		while (s2[j] != '\0')
 		{
-			s2_len++;
+			new_str[i] = s2[j];
+			j++;
+			i++;
 		}
-
-		new_str = (char *) malloc(sizeof(char) * (s2_len + s1_len + 1));
-
-		if (new_str != NULL)
-		{
-			for (i = 0; i < s1_len; i++)
-			{
-				new_str[i] = s1[i];
-			}
-
-			j = 0;
-
-			while (s2[j] != '\0')
-			{
-				new_str[i] = s2[j];
-				j++;
-				i++;
-			}
-			new_str[i] = '\0';
-			return (new_str);
-		}
-		return (NULL);
+		new_str[i] = '\0';
+		return (new_str);
 	}
 	return (NULL);
 }
