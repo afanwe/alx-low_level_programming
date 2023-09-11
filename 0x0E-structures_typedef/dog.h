2ca516d84dc5d344e75c27ef11e890ef966d0285
Asande Didiza <asande.didiza@gmail.com>
2023-09-11 09:22:59 -0700

    added desctription for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - contains variable to hold details about each dog
+ * @name: name of dog
+ * @age: how old the dog is
+ * @owner: who owns the name
+ */
+
 struct dog
 {
 	char *name;
