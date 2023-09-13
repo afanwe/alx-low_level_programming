9e8cd3b869fe1698ff05581287f423bcf0153d82
Asande Didiza <asande.didiza@gmail.com>
2023-09-13 10:57:13 -0700

    Created 1-array_iterator.c and added function prototype to the .h file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array
+ * @array: to print elements of 
+ * @size: size of array
+ * @action: pointer to function to give elements of the array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	i = 0;
+
+	while (i < size)
+	{
+		action(*(array + i));
+		i++;
+	}
+}
