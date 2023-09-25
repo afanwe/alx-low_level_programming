e6267c2968e86fe33bc50f4e6b9a221a07d8787e
Asande Didiza <asande.didiza@gmail.com>
2023-09-25 08:41:50 -0700

    Created 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees emory used by linked list
+ * @head: linked list using memory
+ */
+
+void free_listint2(listint_t *head)
+{
+	listint_t *current_node;
+	listint_t *next_node;
+
+	current_node = head;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
+	*head = NULL;
+}
