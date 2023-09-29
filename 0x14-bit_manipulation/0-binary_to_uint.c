94685f363aac7644dddecf59177005b149635a85
Asande Didiza <asande.didiza@gmail.com>
2023-09-29 12:53:59 -0700

    Created c file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: string of ndigit to convert
+ * Return: returns int value of binary
+ */
+
+unsigned int binary_to_uint(const char *b);
+{
+	char ch;
+	unsigned int value;
+
+	value = 0;
+
+	if (b != NULL)
+	{
+		while (*b != '\0')
+		{
+			ch = *b;
+
+			if (ch == '1' || ch == '0')
+				value = (value << 1) | (ch - '0');
+			else
+				return (0);
+			b++;
+		}
+	}
+	else
+		return (0);
+
+	return (value);
+}
