5888c5f9fd2fbbe7672468920d1739d9161cad64
Asande Didiza <asande.didiza@gmail.com>
2023-10-03 02:03:41 -0700

    Created 100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,8 +9,11 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define BUFFER_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void exit_code(int er_code, const char *str, int fl1, int fl2);
 
 #endif
